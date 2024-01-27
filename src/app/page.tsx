66dffc6642d2a4a991812a923adc3f20fe9a6d4d
Diana <sulkova.diana@gmail.com>
2024-01-27 21:38:32 +0100

    init: implement shadcn for UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
-export default function Home () {
-  
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import {RocketIcon} from '@radix-ui/react-icons';
+
+
+export default function Home() {
   return (
     <main>
       <h1>Hello world</h1>
+      <h2>Here we go</h2>
+      <Alert>
+        <RocketIcon className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components and dependencies to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </main>
   );
 }
