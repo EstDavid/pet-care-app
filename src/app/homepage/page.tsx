296c8a24fe09901b6e18fd9d87c46f94f9efa373
David de Esteban <estdav@protonmail.com>
2024-01-30 23:56:57 +0100

    feat: create initial layout and main dashboard elements

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import logo from '@/../public/logo.png';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <section className="bg-brand-bg h-screen flex flex-col justify-center items-center">
-      <Image className=" px-8 mb-8" alt="CADO logo" src={logo}></Image>
+      <Image className="px-8 mb-8" alt="CADO logo" src={logo}></Image>
       <h1 className="text-brand-fg text-3xl">Welcome to CADO</h1>
+      <Link href="/dashboard">Dashboard</Link>
     </section>
   );
 }
