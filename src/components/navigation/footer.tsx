878aebfa76a42d912aba9a84d2d89aa1e098a989
David de Esteban <estdav@protonmail.com>
2024-01-31 16:59:34 +0100

    feat: implement icons in footer

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -1,33 +1,45 @@
 import Link from 'next/link';
+import { buttonVariants } from '../ui/button';
+import { GoHomeFill } from 'react-icons/go';
+import { HiUsers } from 'react-icons/hi';
+import { CiSettings } from 'react-icons/ci';
 
 export default function Footer() {
   const navLinks = [
     {
       name: 'Home',
-      href: 'dashboard'
+      href: 'dashboard',
+      icon: <GoHomeFill size="2em" />
     },
     {
       name: 'Sitters',
-      href: 'dashboard/sitters'
+      href: 'dashboard/sitters',
+      icon: <HiUsers size="2em" />
     },
     {
-      name: 'Account Settings',
-      href: 'dashboard/account-settings'
+      name: 'Settings',
+      href: 'dashboard/account-settings',
+      icon: <CiSettings size="2em" />
     }
   ];
   return (
     <nav className="w-full h-footer-nav bg-brand-bg">
-      <ul className="flex h-full flex-row justify-end items-center px-x-pad-footer container">
+      <ul className="flex h-full justify-between items-center px-x-pad-footer container">
         {navLinks.map((navLink, index) => {
           return (
-            <div key={index} className="flex-1">
-              <Link
-                href={`/${navLink.href}`}
-                className="text-brand-fg h-auto text-2xl"
-              >
+            <Link
+              key={index}
+              href={`/${navLink.href}`}
+              className={`${buttonVariants({
+                variant: 'link',
+                size: 'iconFooter'
+              })}`}
+            >
+              <div className="flex flex-col items-center w-full">
+                {navLink.icon}
                 {navLink.name}
-              </Link>
-            </div>
+              </div>
+            </Link>
           );
         })}
       </ul>
--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className="w-screen h-header-nav bg-brand-bg flex">
       <div className="flex w-full gap-5 justify-between items-center mt-4 px-4 py-2 container">
-        <div className="h-full flex-1">
+        <div className="h-full flex-1 flex justify-start">
           <div className="relative h-full aspect-square">
             <Image
               alt="user photo"
@@ -18,12 +18,9 @@ export default function Header() {
         </div>
         <h1 className="text-brand-fg text-2xl flex-1 text-center">Hi Sarah!</h1>
         <div className="relative h-full flex-1 flex justify-end">
-          <Image
-            alt="CADO logo"
-            src={logo}
-            objectFit="contain"
-            className="h-full w-auto"
-          ></Image>
+          <div className="relative h-full aspect-square flex flex-col justify-center">
+            <Image alt="CADO logo" src={logo}></Image>
+          </div>
         </div>
       </div>
     </div>
