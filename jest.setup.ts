44008188f0ccdde3baf4889ab98f0a73df3c7a74
ootwither <dougal.wallace@gmail.com>
2024-01-31 11:35:53 +0000

    feat: updates models, adds tests and test documentation

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,3 +2,5 @@ import {TextEncoder, TextDecoder} from 'util';
 ('./setup.jest.ts');
 import '@testing-library/jest-dom';
 Object.assign(global, {TextDecoder, TextEncoder});
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env.local' })
