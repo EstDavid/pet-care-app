2c56bb1922543ae3252cd88f6e8e09645788fa71
Diana <sulkova.diana@gmail.com>
2024-02-01 09:23:19 +0100

    feat: add link to dashboard/pet

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import {Button} from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Page() {
   return (
-    <div className="flex">
+    <div className="flex flex-col items-center">
       <h1>This is the dashboard</h1>
+      <Link href="/dashboard/pet">
+        <Button className="mt-10">Add a pet</Button>
+      </Link>
     </div>
   );
 }
