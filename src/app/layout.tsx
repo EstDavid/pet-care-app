84ead5c35c14084d0cc645b148d55700427f2aa5
Goran Levanic <glevanic@gmail.com>
2024-02-01 11:37:46 +0100

    install clerk and wrap ClerkProvider around app
    Co-authored-by: Dougal Wallace <ootwither@users.noreply.github.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'CADO',
-  description: 'Connecting pet owners with loving pet sitters'
+  description: 'Connecting pet owners with loving pet sitters',
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-brand-fg`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-brand-fg`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
