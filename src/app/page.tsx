52844ab9a5a9ac2650fabc5742332275cd1b232d
David de Esteban <estdav@protonmail.com>
2024-02-01 17:17:33 +0100

    style: fix welcome page styling issues and add paw link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import Image from 'next/image';
 import logo from '@/../public/logo.png';
 import Link from 'next/link';
+import { IoMdPaw } from 'react-icons/io';
 
 export default function Home() {
   return (
     <section className="bg-brand-bg h-screen flex flex-col justify-center items-center">
       <Image
-        className="px-8 mb-8 max-w-[480px]"
+        className="px-8 mb-8 w-full sm:max-w-[480px]"
         alt="CADO logo"
         src={logo}
       ></Image>
       <h1 className="text-brand-fg text-3xl">Welcome to CADO</h1>
       <Link className="text-brand-fg mt-3 text-2xl underline" href="/dashboard">
-        Dashboard
+        <div className="flex flex-col items-center">
+          <IoMdPaw size="4em" />
+          {'Tap here to begin'}
+        </div>
       </Link>
     </section>
   );
