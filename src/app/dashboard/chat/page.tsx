b8955f474b0a877732dd2033a8ec9f29ebef1060
Goran Levanic <glevanic@gmail.com>
2024-02-02 12:44:03 +0100

    quick save

--- src/app/dashboard/chat/page.tsx
+++ src/app/dashboard/chat/page.tsx
@@ -1,47 +1,47 @@
-// import dbConnect from '@/app/lib/dbConnect';
-import Message from '@/lib/db/models/Message';
 import { Card } from '@/components/ui/card';
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
-import postMessage from '@/lib/actions/chat-actions';
-import { getUserMessages } from '@/lib/db/controller/User';
+
 import { auth } from '@clerk/nextjs';
+import { Message } from '@/lib/db/models/Message';
+import { User } from '@/lib/db/models/User';
+import postMessage from '@/lib/actions/chat-actions';
+import { getUserMessages, getUserByClerkId } from '@/lib/db/controller/User';
+import { ObjectId } from 'mongoose';
 
 export default async function Chat() {
-  // await dbConnect();
-  // let messages;
-  // try {
-  //   messages = await Message.find({});
-
-  //   console.log(messages);
-  // } catch (e) {
-  //   console.error(e);
-  // }
-
-  // const messages = await getMessagesByConversationIdFromDB ()
   const { userId }: { userId: string | null } = auth();
-  // let dbUser;
-  // if (userId) dbUser = await getUserByClerkId(user?.id)
+  let dbUser: User | undefined;
+  let messages: Message[] | undefined = [];
+
+  if (userId) {
+    dbUser = await getUserByClerkId(userId);
+    if (dbUser && dbUser._id)
+      messages = await getUserMessages(dbUser._id.toString());
+  }
 
   return (
     <div className="flex flex-col justify-between items-center">
       <div className="flex flex-col overflow-y-auto my-4">
         {messages &&
-          messages.map((message: MessageType) => {
+          messages.map((message: Message) => {
             return (
               <div
+                key={message.id}
                 className={`flex ${
-                  message.sender === userId ? 'justify-end' : 'justify-start'
+                  message.sender?.toString() === userId
+                    ? 'justify-end'
+                    : 'justify-start'
                 }`}
               >
-                <Card key={message._id} className="px-2 my-2 w-4/5">
+                <Card className="px-2 my-2 w-4/5">
                   <p className="text-base py-1">{message.textContent}</p>
 
                   <p className="text-xs text-right text-slate-400 leading-4">
-                    {new Date(message.createdAt).toLocaleString([], {
+                    {/* {new Date(message.createdAt).toLocaleString([], {
                       hour: '2-digit',
                       minute: '2-digit',
-                    })}
+                    })} */}
                   </p>
                 </Card>
               </div>
