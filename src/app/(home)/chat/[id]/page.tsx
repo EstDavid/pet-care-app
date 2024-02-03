380a75ed006b5a68b07647ed528f135d2d2b2c9c
Goran Levanic <glevanic@gmail.com>
2024-02-03 16:43:04 +0100

    fix: fix alignment of messages and width of messages

--- src/app/(home)/chat/[id]/page.tsx
+++ src/app/(home)/chat/[id]/page.tsx
@@ -32,34 +32,32 @@ export default async function Chat({ params }: { params: { id: string } }) {
   const { messages } = conversation;
 
   return (
-    <div className="flex flex-col justify-between items-center">
-      <div className="flex flex-col overflow-y-auto my-4">
+    <div className="flex flex-col justify-between h-full ">
+      <div className="flex flex-col overflow-y-auto gap-4">
         {messages &&
-          messages.map((message: IMessage) => {
-            return (
-              <div
-                key={message.id}
-                className={`flex ${
-                  message.sender?.toString() === userId
-                    ? 'justify-end'
-                    : 'justify-start'
-                }`}
-              >
-                <Card className="px-2 my-2 w-4/5">
-                  <p className="text-base py-1">{message.textContent}</p>
+          messages.map((message: IMessage) => (
+            <div
+              key={message.id}
+              className={`flex ${
+                message.sender?.toString() === dbUser._id?.toString()
+                  ? 'justify-end'
+                  : 'justify-start'
+              }`}
+            >
+              <Card className="px-2 my-2 min-w-[10%] max-w-[80%] bg-brand-bg-50">
+                <p className="py-1">{message.textContent}</p>
 
-                  <p className="text-xs text-right text-slate-400 leading-4">
-                    {new Date(message.createdAt).toLocaleString([], {
-                      hour: '2-digit',
-                      minute: '2-digit'
-                    })}
-                  </p>
-                </Card>
-              </div>
-            );
-          })}
+                <p className="text-xs text-right text-slate-500">
+                  {new Date(message.createdAt).toLocaleString([], {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                  })}
+                </p>
+              </Card>
+            </div>
+          ))}
       </div>
-      <div className="grid w-full gap-2">
+      <div className="grid w-full gap-2 mt-4">
         <form action={postNewMessage}>
           <MessageForm />
         </form>
