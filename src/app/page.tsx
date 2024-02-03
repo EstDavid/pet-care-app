10f18af977e3f313df3243fd8fc0dc652df78a31
David de Esteban <estdav@protonmail.com>
2024-02-03 17:33:08 +0100

    feat: initial implementation of redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from 'next/image';
 import logo from '@/../public/logo.png';
 import Link from 'next/link';
 import { IoMdPaw } from 'react-icons/io';
+import { getUser } from '@/lib/utils';
 
-export default function Home() {
+export default async function Home() {
+  const user = await getUser();
+
+  const href = user ? '/dashboard' : 'chooseRole';
+
   return (
     <section className="bg-brand-bg h-screen flex flex-col justify-center items-center">
       <Image
@@ -13,7 +18,7 @@ export default function Home() {
         priority={true}
       ></Image>
       <h1 className="text-brand-fg text-3xl">Welcome to CADO</h1>
-      <Link className="text-brand-fg mt-3 text-2xl underline" href="/chooseRole">
+      <Link className="text-brand-fg mt-3 text-2xl underline" href={href}>
         <div className="flex flex-col items-center">
           <IoMdPaw size="4em" />
           {'Tap here to begin'}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { auth } from "@clerk/nextjs";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { getUserByClerkId } from "./db/controller/User";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn (...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export async function getUser () {
+  const { userId } = auth();
+
+  if (userId) {
+    const user = await getUserByClerkId(userId);
+
+    return user;
+  }
+
+  return undefined;
 }
