82ba4ba8150997b5542e66b2af7d772d0572b861
Goran Levanic <glevanic@gmail.com>
2024-02-04 23:10:05 +0100

    chore: move messages in its own component to enable hooks

--- src/app/(home)/chat/[id]/page.tsx
+++ src/app/(home)/chat/[id]/page.tsx
@@ -1,15 +1,15 @@
-import { Card } from '@/components/ui/card';
-
 import { auth } from '@clerk/nextjs';
-import { IMessage } from '@/lib/db/models/Message';
 import postMessage from '@/lib/actions/chat-actions';
 import { getUserByClerkId } from '@/lib/db/controller/User';
 import { getConversationById } from '@/lib/db/controller/Conversation';
 import { notFound } from 'next/navigation';
 import MessageForm from '@/components/message-form';
+import ChatMessages from '@/components/chat-messages';
 
 export default async function Chat({ params }: { params: { id: string } }) {
   const { userId }: { userId: string | null } = auth();
+
+  // Retrieve conversation from DB, conversationId is passed as a parameter
   const { id: conversationId } = params;
   const conversation = await getConversationById(conversationId);
 
@@ -17,49 +17,39 @@ export default async function Chat({ params }: { params: { id: string } }) {
     return notFound();
   }
 
+  // Get the user from the database
   const dbUser = await getUserByClerkId(userId);
 
   if (!dbUser || !dbUser._id) {
     return notFound();
   }
 
+  // Pass conversationId and dbUser as additional arguments to postMessage server action
   const postNewMessage = postMessage.bind(
     null,
     conversationId,
     dbUser._id.toString()
   );
 
+  // Retrieve messages from the conversation object
   const { messages } = conversation;
+
+  // Convert messages object to JSON to be able to pass it as a prop
+  const messagesObject = JSON.parse(JSON.stringify(messages));
 
   return (
     <div className="flex flex-col justify-between h-full ">
-      <div className="flex flex-col overflow-y-auto gap-4">
-        {messages &&
-          messages.map((message: IMessage) => (
-            <div
-              key={message.id}
-              className={`flex ${
-                message.sender?.toString() === dbUser._id?.toString()
-                  ? 'justify-end'
-                  : 'justify-start'
-              }`}
-            >
-              <Card className="px-2 my-2 min-w-[10%] max-w-[80%] bg-brand-bg-50">
-                <p className="py-1">{message.textContent}</p>
-
-                <p className="text-xs text-right text-slate-500">
-                  {new Date(message.createdAt).toLocaleString([], {
-                    hour: '2-digit',
-                    minute: '2-digit',
-                  })}
-                </p>
-              </Card>
-            </div>
-          ))}
-      </div>
+      <ChatMessages
+        messages={messagesObject}
+        conversationId={conversationId}
+        dbUser={dbUser._id.toString()}
+      />
       <div className="grid w-full gap-2 mt-4">
         <form action={postNewMessage}>
-          <MessageForm />
+          <MessageForm
+            conversationId={conversationId}
+            dbUser={dbUser._id.toString()}
+          />
         </form>
       </div>
     </div>
