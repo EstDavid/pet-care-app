4d1e1894cbf3378b44497534aa3c86c57694a4fd
Goran Levanic <glevanic@gmail.com>
2024-02-04 23:08:51 +0100

    feat: set up socket.io server

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000', // Frontend URL
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true,
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  // Join a conversation room
+  socket.on('join_room', (conversationId) => {
+    socket.join(conversationId);
+  });
+
+  // Send a message to a conversation room
+  socket.on('send_msg', async (data, conversationId) => {
+    console.log(data, 'DATA');
+    console.log(conversationId, 'CONVERSATION ID');
+    io.to(conversationId).emit('receive_msg', data);
+  });
+  //
+
+  // Leave a conversation room
+  socket.on('disconnect', () => {
+    console.log('A user disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+httpServer.listen(PORT, () => {
+  console.log(`Socket.io server is running on port ${PORT}`);
+});
+
+export default io;
