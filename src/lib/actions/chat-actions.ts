858a110d86b1dac25d420a59fbf14e778e1b337d
Goran Levanic <glevanic@gmail.com>
2024-02-04 23:14:02 +0100

    feat: add postImage server action to save images to DB

--- src/lib/actions/chat-actions.ts
+++ src/lib/actions/chat-actions.ts
@@ -3,23 +3,36 @@ import { revalidatePath } from 'next/cache';
 import Message from '@/lib/db/models/Message';
 import { postMessageToConversation } from '../db/controller/Conversation';
 
-export default async function postMessage (
+export default async function postMessage(
   conversationId: string,
   senderId: string,
   formData: FormData
 ) {
-
   try {
     const newMessage = new Message({
       textContent: formData.get('message') as string,
-      sender: senderId
+      sender: senderId,
     });
 
-    await postMessageToConversation(
-      conversationId,
-      newMessage
-    );
+    await postMessageToConversation(conversationId, newMessage);
+  } catch (error) {
+    console.log(error);
+  }
+  revalidatePath(`/chat/${conversationId}`);
+}
 
+export async function postImage(
+  conversationId: string,
+  senderId: string,
+  mediaUrl: string
+) {
+  try {
+    const newMessage = new Message({
+      mediaUrl,
+      sender: senderId,
+    });
+
+    await postMessageToConversation(conversationId, newMessage);
   } catch (error) {
     console.log(error);
   }
