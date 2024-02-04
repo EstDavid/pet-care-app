80bd3f6776d46c1c1390381e2a0a4cb6021808f8
Goran Levanic <glevanic@gmail.com>
2024-02-04 23:13:32 +0100

    feat: add socket connection, image upload logic

--- src/components/message-form.tsx
+++ src/components/message-form.tsx
@@ -3,19 +3,55 @@
 import { ChangeEvent, useState } from 'react';
 import { Button } from './ui/button';
 import { Textarea } from './ui/textarea';
+import io from 'socket.io-client';
+import UploadWidget from '@/components/upload-widget';
+import { postImage } from '@/lib/actions/chat-actions';
 
-export default function MessageForm() {
+// Connect to the socket server
+var socket: any;
+socket = io('http://localhost:3001');
+
+export default function MessageForm({
+  conversationId,
+  dbUser,
+}: {
+  conversationId: string;
+  dbUser: string;
+}) {
   const [textarea, setTextarea] = useState('');
+  const [mediaUrl, setMediaUrl] = useState('');
 
+  // Send image to socket server and post it to the database
+  const handleImageUpload = (result: string) => {
+    setMediaUrl(result);
+    const newMessage = {
+      textContent: '',
+      createdAt: new Date().toISOString(),
+      sender: dbUser,
+      mediaUrl: result,
+    };
+    socket.emit('send_msg', newMessage, conversationId);
+    postImage(conversationId, dbUser, result);
+    setMediaUrl('');
+  };
+
+  // Send message to socket server post it to the database
   const handleSubmit = (
     event:
       | React.KeyboardEvent<HTMLTextAreaElement>
       | React.MouseEvent<HTMLButtonElement>
   ) => {
+    const newMessage = {
+      textContent: textarea,
+      createdAt: new Date().toISOString(),
+      sender: dbUser,
+    };
+    socket.emit('send_msg', newMessage, conversationId);
     setTextarea('');
     event.currentTarget.form?.requestSubmit();
   };
 
+  // Send message when the user presses the Enter key
   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key === 'Enter' || e.key === 'NumpadEnter') {
       e.preventDefault();
@@ -24,6 +60,7 @@ export default function MessageForm() {
   };
 
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    // TODO not possible to delete last character
     if (event.target && event.target.value) {
       setTextarea(event.target.value);
     }
@@ -38,10 +75,12 @@ export default function MessageForm() {
         value={textarea}
         onKeyDown={handleKeyDown}
         onChange={handleChange}
+        className="bg-white"
       />
       <Button onClick={handleSubmit} disabled={textarea === ''}>
         Send message
       </Button>
+      <UploadWidget onUploadedSuccess={handleImageUpload} />
     </div>
   );
 }
