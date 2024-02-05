26d5f21c0b8a4691bb4494b4a892728c8d3db385
David de Esteban <estdav@protonmail.com>
2024-02-05 22:27:34 +0100

    feat: implement Stays and Messages links in footer

--- src/components/navigation/footer.tsx
+++ src/components/navigation/footer.tsx
@@ -2,31 +2,16 @@
 import Link from 'next/link';
 import { buttonVariants } from '../ui/button';
 import { GoHomeFill } from 'react-icons/go';
-import { HiUsers } from 'react-icons/hi';
 import { CiSettings } from 'react-icons/ci';
 import { currentUser } from '@clerk/nextjs';
-import { checkUserRole, getUserByClerkId } from '@/lib/db/controller/User';
+import { checkUserRole } from '@/lib/db/controller/User';
+import { FaDog } from 'react-icons/fa';
 import { FaRegMessage } from 'react-icons/fa6';
-import { ColumnSpacingIcon } from '@radix-ui/react-icons';
-import { getUnreadMessages } from '@/lib/db/controller/Conversation';
-import { notFound } from 'next/navigation';
 
 export default async function Footer() {
   const clerkUser = await currentUser();
   const clerkUserId = clerkUser?.id;
   const role = await checkUserRole(clerkUserId || '');
-
-  if (!clerkUserId) {
-    return notFound();
-  }
-
-  const user = await getUserByClerkId(clerkUserId);
-
-  if (!user || !user._id) {
-    return notFound();
-  }
-
-  const messages = await getUnreadMessages(user._id.toString());
 
   let navLinks: { name: string; href: string; icon: JSX.Element }[] = [];
   if (role === 'owner') {
@@ -37,8 +22,13 @@ export default async function Footer() {
         icon: <GoHomeFill size="2em" />
       },
       {
+        name: 'Stays',
+        href: 'stays',
+        icon: <FaDog size="2em" />
+      },
+      {
         name: 'Messages',
-        href: 'owner/sitters',
+        href: 'chat',
         icon: <FaRegMessage size="2em" />
       },
       {
@@ -55,8 +45,13 @@ export default async function Footer() {
         icon: <GoHomeFill size="2em" />
       },
       {
+        name: 'Stays',
+        href: 'stays',
+        icon: <FaDog size="2em" />
+      },
+      {
         name: 'Messages',
-        href: 'sitter/dashboard',
+        href: 'chat',
         icon: <FaRegMessage size="2em" />
       },
       {
