65e38b453c3bcdc04d2dc192d10c63cf5732aa5f
David de Esteban <estdav@protonmail.com>
2024-02-06 17:13:39 +0100

    feat: redirect user to '/' page after signout

--- src/app/(home)/user-profile/page.tsx
+++ src/app/(home)/user-profile/page.tsx
@@ -1,9 +1,10 @@
+'use server';
 import UserProfile from '@/components/user-profile';
 import { getUserByClerkId } from '@/lib/db/controller/User';
 import { notFound } from 'next/navigation';
-import { SignOutButton, currentUser } from '@clerk/nextjs';
-import { PiSignOut } from 'react-icons/pi';
+import { currentUser } from '@clerk/nextjs';
 import Subheader from '@/components/navigation/subheader';
+import CustomSignoutButton from '@/components/navigation/customSignoutButton';
 
 export default async function Page() {
   const clerkUser = await currentUser();
@@ -11,21 +12,16 @@ export default async function Page() {
   const rawUserData = await getUserByClerkId(clerkUserId || '');
   // serializing the user object to avoid circular structure error
   const user = JSON.parse(JSON.stringify(rawUserData));
+  // const router = useRouter();
 
   if (!user) {
     notFound();
   }
+
   return (
     <div className="flex flex-col items-center gap-4">
       <Subheader title="Manage profile">
-        <SignOutButton>
-          <div className=" 0 border-brand-cta-950 border-2 w-full h-full flex justify-end px-3 py-2 rounded-md cursor-pointer">
-            <div className="h-full w-full flex justify-end items-center gap-3 text-brand-cta-950">
-              <p className="text-xl">Signout</p>
-              <PiSignOut size="2em" />
-            </div>
-          </div>
-        </SignOutButton>
+        <CustomSignoutButton />
       </Subheader>
       <div className="mt-subheader-height">
         <UserProfile user={user} />
