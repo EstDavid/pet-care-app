e70e8bf9bd300fa74868ac33d8409b14b7091a95
David de Esteban <estdav@protonmail.com>
2024-02-06 15:29:59 +0100

    feat: add subheader and change size to avatar image

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -1,16 +1,15 @@
-"use server";
-import logo from "@/../public/logo.png";
-import Image from "next/image";
-import { currentUser } from "@clerk/nextjs";
-import { getUserByClerkId } from "@/lib/db/controller/User";
-import userimg from "@/../public/mock-user-photo.jpg";
+'use server';
+import logo from '@/../public/logo.png';
+import Image from 'next/image';
+import { currentUser } from '@clerk/nextjs';
+import { getUserByClerkId } from '@/lib/db/controller/User';
 
 export default async function Header() {
   const clerkUser = await currentUser();
   const clerkName = clerkUser?.firstName;
   const clerkImageUrl = clerkUser?.imageUrl;
 
-  const dbUser = await getUserByClerkId(clerkUser?.id || "");
+  const dbUser = await getUserByClerkId(clerkUser?.id || '');
   const dbImageUrl = dbUser?.pfpUrl;
 
   // if there is no image in the db uploaded by user, use the clerk image
@@ -20,19 +19,15 @@ export default async function Header() {
   return (
     <div className="w-screen h-header-nav bg-brand-bg flex">
       <div className="flex w-full gap-5 justify-between items-center mt-4 px-4 py-2 container">
-        <div className="h-full flex-1 flex justify-start">
-          <div className="relative h-full aspect-square">
-            <Image
-              alt="user photo"
-              src={imageUrl || ""}
-              fill={true}
-              sizes="100px"
-              style={{
-                objectFit: "cover",
-              }}
-              className="rounded-full"
-            ></Image>
-          </div>
+        <div className="h-full flex justify-start items-center">
+          <Image
+            alt="user photo"
+            src={imageUrl || ''}
+            width={0}
+            height={0}
+            sizes="100vh"
+            className="w-header-photo h-header-photo object-cover rounded-full"
+          ></Image>
         </div>
         <h1 className="text-brand-fg font-semibold flex-1 text-xl text-center">
           Hi {clerkName}!
