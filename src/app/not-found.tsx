ca7177eba73475f03e1b8aa82d2504b57e272586
Diana <sulkova.diana@gmail.com>
2024-02-06 17:04:27 +0100

    feat: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <section className="bg-brand-bg h-screen flex flex-col justify-center items-center">
+      <h1 className="text-brand-fg text-xl">404 | Page not found 😿</h1>
+    </section>
+  );
+}
