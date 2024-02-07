ca2e56dc9313f46a03cc8fb99a17f6de771b20db
ootwither <dougal.wallace@gmail.com>
2024-02-07 14:51:27 +0000

    feat: implements drawer component

--- src/components/sitter/stay-card.tsx
+++ src/components/sitter/stay-card.tsx
@@ -7,6 +7,17 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
 import { Button } from '@/components/ui/button';
 import { FullStay } from '@/lib/db/models/Stay'
 import { useState } from 'react';
@@ -28,40 +39,85 @@ export default function StayCard({ stay, role }: { stay: FullStay, role: string 
   }
 
   return (
-    <>
+    <><Drawer>
       <Card>
         <div className='flex w-full flex-col'>
           <div className='flex w-full'>
             <Image src={stay.owner.pfpUrl || 'https://res.cloudinary.com/cw-app/image/upload/v1707210944/pet-app/bttjaerjdctmwp1b2abr.jpg'}
               alt={stay.owner.firstname || 'photo of owner'}
-              width={0} height={0} sizes='200px' className='w-[50px] h-[50px] rounded-full'
+              width={0} height={0} sizes='200px' className='w-[50px] h-[50px] rounded-full m-1'
             />
             <p>{`${stay.owner.firstname} / ${stay.owner.contact?.city}`}</p>
             <p>From :{new Date(stay.from).toLocaleDateString('de-AT')}</p>
             <p>To :{new Date(stay.to).toLocaleDateString('de-AT')}</p>
           </div>
 
-          <div className='flex flex-col gap-3'>
+          <div className='flex flex-col gap-1'>
             {stay.pet.map((onePet) => {
-              return (
-                <div key={onePet.name} className='flex flex-row gap-8 m-2 content-center justify-around border'>
-                  <div>
-                    {onePet.pfpUrl && // pet profile pic or icon
-                      <Image src={onePet.pfpUrl || 'https://res.cloudinary.com/cw-app/image/upload/v1707210944/pet-app/bttjaerjdctmwp1b2abr.jpg'}
-                        alt={stay.owner.firstname || 'photo of owner'}
-                        width={0} height={0} sizes='200px' className='w-[50px] h-[50px] rounded-full'
-                      />
-                    }
-                    {!onePet.pfpUrl && onePet.species == 'dog' ? <PiDogFill /> : <PiCatFill />}
+              return (<div key={onePet.name}>
+                <DrawerTrigger key={onePet._id?.toString()}>
+                  <div key={onePet.name} className='flex flex-row gap-3 m-2 content-center justify-evenly border'>
+                    <div>
+                      {onePet.pfpUrl && // pet profile pic or icon
+                        <Image src={onePet.pfpUrl || 'https://res.cloudinary.com/cw-app/image/upload/v1707210944/pet-app/bttjaerjdctmwp1b2abr.jpg'}
+                          alt={stay.owner.firstname || 'photo of owner'}
+                          width={0} height={0} sizes='200px' className='w-[50px] h-[50px] rounded-full'
+                        />
+                      }
+                      {!onePet.pfpUrl && onePet.species == 'dog' ? <PiDogFill /> : <PiCatFill />}
+                    </div>
+                    <p className='p-1 rounded-sm bg-brand-bg-200'>{onePet.name}</p>
+                    <p className='p-1 rounded-sm bg-brand-bg-200 text-nowrap'>Age: {onePet.age}</p>
+                    <p className='p-1 rounded-sm bg-brand-bg-200'>{onePet.sex[0].toUpperCase() + onePet.sex.substring(1)}</p>
+                    <p className='p-1 rounded-sm bg-brand-bg-600'>{onePet.sprayed ? 'SPAYED' : 'INTACT'}</p>
                   </div>
-                  <p>{onePet.name}</p>
-                  <p className='text-nowrap'>Age: {onePet.age}</p>
-                  <p>{onePet.sex[0].toUpperCase() + onePet.sex.substring(1)}</p>
-                  <p className='rounded-sm p-1 bg-brand-bg-600'>{onePet.sprayed ? 'SPAYED' : 'INTACT'}</p>
-                </div>
+                </DrawerTrigger>
+                <DrawerContent>
+                  <div className="mx-auto w-full max-w-sm">
+                    <DrawerHeader>
+                      <DrawerTitle>{onePet.name}</DrawerTitle>
+                      <DrawerDescription>{onePet.age} year old {onePet.sex} {onePet.breed}</DrawerDescription>
+                    </DrawerHeader>
+                    <div className='flex flex-col content-evenly'>
+                      <div className='flex flex-row items-center gap-2 content-evenly'>
+                        <p>Behaviour notes:</p> <p>{onePet.notes || 'None'}</p>
+                      </div>
+                      <div className='flex flex-row items-center gap-2 content-evenly'>
+                        <p>Allergies:</p> <p>{onePet.allergies || 'None'}</p>
+                      </div>
+                      <div className='flex flex-row items-center gap-2 content-evenly'>
+                        <p>Medication:</p> <p>{onePet.medication || 'None'}</p>
+                      </div>
+                      <div className='flex flex-row items-center gap-2 content-evenly'>
+                        <p>Vaccinations:</p> <p>{onePet.vaccinations || 'None'}</p>
+                      </div>
+                      <div className='flex flex-row items-center gap-2 content-evenly'>
+                        <p>Vaccinations:</p> <p>{onePet.vaccinations || 'None'}</p>
+                      </div>
+                      <div className='flex flex-row items-center gap-2 content-evenly'>
+                        <p>Emergency instructions:</p> <p>{onePet.emergencyInstructions || 'None'}</p>
+                      </div>
+                      <div className='flex flex-row items-center gap-2 content-evenly'>
+                        <p>Insurance details:</p> <p>{onePet.insurance || 'None'}</p>
+                      </div>
+                      <div className='flex flex-row items-center gap-2 content-evenly'>
+                        <p>Microchip number:</p> <p>{onePet.microchip || 'None'}</p>
+                      </div>
+
+                    </div>
+                    <DrawerFooter>
+                      <DrawerClose asChild>
+                        <Button variant="outline">Done</Button>
+                      </DrawerClose>
+                    </DrawerFooter>
+                  </div>
+                </DrawerContent>
+              </div>
               )
             })}
           </div>
+
+
           <div className='gap-2 flex flex-col m-2'>
             <Button>CONTACT OWNER</Button>
             {!confirmed && <Button onClick={handleConfirm}>CONFIRM</Button>}
@@ -69,5 +125,7 @@ export default function StayCard({ stay, role }: { stay: FullStay, role: string 
           </div>
         </div>
       </Card>
+
+    </Drawer>
     </>);
 }
