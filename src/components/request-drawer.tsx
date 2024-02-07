2f2db33ea39bd6b095001ab444349030b98c6c25
Goran Levanic <glevanic@gmail.com>
2024-02-07 15:52:52 +0100

    feat: close drawer on form submit

--- src/components/request-drawer.tsx
+++ src/components/request-drawer.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Calendar } from '@/components/ui/calendar';
 import { DateRange } from 'react-day-picker';
 import { cn } from '@/lib/utils';
@@ -10,15 +10,15 @@ import {
 } from '@/components/ui/popover';
 import { Button } from './ui/button';
 import { CalendarIcon } from '@radix-ui/react-icons';
-import { addDays, format } from 'date-fns';
+import { format } from 'date-fns';
 import {
   DrawerClose,
-  DrawerDescription,
   DrawerFooter,
   DrawerHeader,
   DrawerTitle,
 } from '@/components/ui/drawer';
 import { Checkbox } from '@/components/ui/checkbox';
+import { Separator } from '@/components/ui/separator';
 import requestStay from '@/lib/actions/request-stay';
 import { Pet } from '@/lib/db/models/Pet';
 
@@ -35,6 +35,7 @@ export default function RequestDrawer({
   const [selectedPets, setSelectedPets] = useState<boolean[]>(
     Array(pets.length).fill(false)
   );
+  const closeButtonRef = useRef<HTMLButtonElement>(null);
 
   function handlePetSelection(index: number) {
     setSelectedPets((prev) => {
@@ -44,23 +45,33 @@ export default function RequestDrawer({
     });
   }
 
-  console.log(selectedPets);
   let infoComplete = date?.to !== undefined && selectedPets.includes(true);
 
   const from = date?.from?.toLocaleDateString() as string;
   const to = date?.to?.toLocaleDateString() as string;
 
-  const submitForm = requestStay.bind(null, owner, sitter, from, to);
+  // Pass additional arguments to server action
+  const actionFunction = requestStay.bind(null, owner, sitter, from, to);
+
+  // Call the server action and close the drawer
+  const newActionFunction = (formData: FormData) => {
+    actionFunction(formData);
+    if (closeButtonRef.current) {
+      closeButtonRef.current.click();
+    }
+  };
 
   return (
-    <form action={submitForm}>
+    <form action={newActionFunction}>
       <DrawerHeader>
-        <DrawerTitle className="mb-4">Request a stay</DrawerTitle>
-        <div className="flex flex-col items-center w-full gap-2">
+        <DrawerTitle className="mb-1">Request a stay</DrawerTitle>
+        <Separator />
+        <div className="flex flex-col items-center w-full gap-2 mt-2">
           {/* Render Pet checkboxes */}
-          <div className="flex gap-4">
+          <label>Choose pets:</label>
+          <div className="flex flex-col gap-4">
             {pets?.map((pet: any, index: number) => (
-              <div className="flex gap-1" key={pet._id}>
+              <div className="flex gap-2" key={pet._id}>
                 <Checkbox
                   id={pet._id}
                   name={pet._id}
@@ -98,7 +109,7 @@ export default function RequestDrawer({
                     format(date.from, 'LLL dd, y')
                   )
                 ) : (
-                  <span>Pick a date</span>
+                  <span>Pick a date range</span>
                 )}
               </Button>
             </PopoverTrigger>
@@ -121,7 +132,12 @@ export default function RequestDrawer({
           Submit
         </Button>
         <DrawerClose>
-          <Button type="button" variant="outline" className="w-full">
+          <Button
+            type="button"
+            variant="outline"
+            className="w-full"
+            ref={closeButtonRef}
+          >
             Close
           </Button>
         </DrawerClose>
