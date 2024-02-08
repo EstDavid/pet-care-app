c12078a539e50adfaa1f005f9521dcd9cf74724c
Goran Levanic <glevanic@gmail.com>
2024-02-08 09:24:45 +0100

    feat: fetching stays from a user

--- src/app/(home)/owner/lala/page.tsx
+++ src/app/(home)/owner/lala/page.tsx
@@ -1,5 +1,22 @@
 import OwnerUpdates from '@/components/owner-updates';
+import { currentUser } from '@clerk/nextjs';
+import { getStaysByClerkUser } from '@/lib/db/controller/Stay';
 
-export default function Updates() {
+export default async function Updates() {
+  const clerkUser = await currentUser();
+
+  if (!clerkUser) {
+    return null;
+  }
+
+  const stays = await getStaysByClerkUser(clerkUser.id);
+  console.log(stays);
+
+  if (clerkUser) {
+    const stays = await getStaysByClerkUser(clerkUser.id);
+  }
+  // stays.forEach((stay) => {
+  // console.log(stays);
+  // });
   return <OwnerUpdates />;
 }
