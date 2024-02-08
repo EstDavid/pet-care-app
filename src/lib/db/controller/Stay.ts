fc5850de6363df2bc958ea94e123a5e58d753ed1
Diana <sulkova.diana@gmail.com>
2024-02-08 01:26:08 +0100

    feat: implement backend and send updates component

--- src/lib/db/controller/Stay.ts
+++ src/lib/db/controller/Stay.ts
@@ -1,10 +1,11 @@
 import dbConnect from '../dbConnect';
-import User, { User as IUser } from '../models/User';
-import Pet, { Pet as IPet } from '../models/Pet';
-import Stay, { FullStay, Stay as IStay } from '../models/Stay';
-import { ObjectId, Types } from 'mongoose';
+import User, {User as IUser} from '../models/User';
+import Pet, {Pet as IPet} from '../models/Pet';
+import Stay, {FullStay, Stay as IStay} from '../models/Stay';
+import {ObjectId, Types} from 'mongoose';
+import Update, {Update as IUpdate} from '../models/Update';
 
-export async function addStay (
+export async function addStay(
   owner: string,
   sitter: string,
   petArray: string[],
@@ -29,12 +30,12 @@ export async function addStay (
   }
 }
 
-export async function confirmStay (
+export async function confirmStay(
   _id: Types.ObjectId
 ): Promise<IStay | undefined> {
   await dbConnect();
   try {
-    const result = await Stay.findOneAndUpdate({ _id }, { confirmed: true });
+    const result = await Stay.findOneAndUpdate({_id}, {confirmed: true});
     if (result) return result;
     else throw new Error('error, stay confirm failed');
   } catch (e) {
@@ -43,13 +44,13 @@ export async function confirmStay (
 }
 
 // Added by Alaa Starts here
-export async function getStaysForPet (
+export async function getStaysForPet(
   petId: string
 ): Promise<IStay[] | undefined> {
   await dbConnect();
 
   try {
-    const stays = await Stay.find({ pet: petId });
+    const stays = await Stay.find({pet: petId});
     return stays || []; // Return an empty array if there are no stays
   } catch (e) {
     console.error(e);
@@ -58,27 +59,29 @@ export async function getStaysForPet (
 }
 
 // If no date is passed, it return all the stays. Otherwise it only return future or ongoing stays
-export async function getStaysByUser (userId: Types.ObjectId, currentDate?: Date): Promise<FullStay[]> {
+export async function getStaysByUser(
+  userId: Types.ObjectId,
+  currentDate?: Date
+): Promise<FullStay[]> {
   await dbConnect();
 
   try {
     const staysQuery = Stay.find({
-      $or: [
-        { owner: userId },
-        { sitter: userId }
-      ],
+      $or: [{owner: userId}, {sitter: userId}],
     });
 
     if (currentDate) {
-      staysQuery.find({ to: { $gte: currentDate } });
+      staysQuery.find({to: {$gte: currentDate}});
     }
 
     staysQuery
-      .populate({ path: 'owner', model: User })
-      .populate({ path: 'sitter', model: User })
-      .populate({ path: 'pet', model: Pet });
+      .populate({path: 'owner', model: User})
+      .populate({path: 'sitter', model: User})
+      .populate({path: 'pet', model: Pet})
+      .populate({path: 'updates', model: Update});
 
-    const stays: FullStay[] = await staysQuery.exec() as unknown as FullStay[];
+    const stays: FullStay[] =
+      (await staysQuery.exec()) as unknown as FullStay[];
 
     return stays || []; // Return an empty array if there are no stays
   } catch (e) {
@@ -87,17 +90,17 @@ export async function getStaysByUser (userId: Types.ObjectId, currentDate?: Date
   }
 }
 
-
 /**pass it a clerk ID, get a list of stays */
-export async function getStaysByClerkUser (clerkId: string) {
+export async function getStaysByClerkUser(clerkId: string) {
   await dbConnect();
 
   try {
-    const user = await User.findOne({ clerkID: clerkId });
+    const user = await User.findOne({clerkID: clerkId});
 
-    const stays = await Stay.find({ sitter: user._id })
-      .populate<{ pet: IPet[]; }>({ path: 'pet', model: Pet })
-      .populate<{ owner: IUser; }>({ path: 'owner', model: User });
+    const stays = await Stay.find({sitter: user?._id})
+      .populate<{pet: IPet[]}>({path: 'pet', model: Pet})
+      .populate<{owner: IUser}>({path: 'owner', model: User})
+      .populate<{updates: IUpdate[]}>({path: 'updates', model: Update});
 
     return stays || []; // Return an empty array if there are no stays
   } catch (e) {
@@ -107,11 +110,11 @@ export async function getStaysByClerkUser (clerkId: string) {
 }
 
 // Confirm Stay is Eneded
-export async function confirmStayIsEnded (stayId: string): Promise<boolean> {
+export async function confirmStayIsEnded(stayId: string): Promise<boolean> {
   await dbConnect();
 
   try {
-    const stay = await Stay.findOne({ _id: stayId });
+    const stay = await Stay.findOne({_id: stayId});
     if (!stay) return false;
     const today = new Date();
     return today > stay.to;
@@ -122,7 +125,7 @@ export async function confirmStayIsEnded (stayId: string): Promise<boolean> {
 }
 
 // Check if isPetInStay by checking is stay is there and if stay not ended
-export async function isPetInStay (petId: string): Promise<boolean> {
+export async function isPetInStay(petId: string): Promise<boolean> {
   await dbConnect();
 
   try {
@@ -137,3 +140,22 @@ export async function isPetInStay (petId: string): Promise<boolean> {
   }
 }
 // Added by Alaa Ends here
+
+export async function postUpdateToStay(
+  id: string,
+  update: IUpdate
+): Promise<IStay | undefined> {
+  try {
+    const newUpdate = await Update.create(update);
+
+    const stay = await Stay.findOneAndUpdate(
+      {_id: id},
+      {$push: {updates: newUpdate._id}},
+      {new: true}
+    );
+
+    return stay?.populate({path: 'updates', model: Update});
+  } catch (error) {
+    console.error(error);
+  }
+}
